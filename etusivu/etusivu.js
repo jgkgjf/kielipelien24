f669a79572f5ecf3fbba1070eff5b6104dbead47
jgkgjf <101794618+jgkgjf@users.noreply.github.com>
2024-02-16 15:20:37 +0200

    Delete etusivu directory

--- etusivu/etusivu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        document.getElementById('welcome-screen').style.opacity = '0';
-        document.getElementById('welcome-screen').style.pointerEvents = 'none';
-    }, 3000);
-});
-
